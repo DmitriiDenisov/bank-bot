a6bbf3c26387baad5139fe626b692a79884bb583
DmitriiDenisov <dmitryhse@gmail.com>
2020-08-28 14:18:44 +0400

    Add mount credentials Optimized Dockerfile_base_worker

--- run_worker_own_transfer.sh
+++ run_worker_own_transfer.sh
@@ -1,2 +1,2 @@
 docker pull dmitrydenisov/worker_base
-docker run --entrypoint python3 -d --net="host" --name $1 dmitrydenisov/worker_base -u rabbitmq_utils/worker_own_transfer.py
+docker run -v $(pwd)/credentials:/app/credentials --entrypoint python3 -d --net="host" --name $1 dmitrydenisov/worker_base -u rabbitmq_utils/worker_own_transfer.py
--- run_worker_transaction.sh
+++ run_worker_transaction.sh
@@ -1,3 +1,3 @@
 docker login
 docker pull dmitrydenisov/worker_base
-docker run --entrypoint python3 -d --net="host" --name $1 dmitrydenisov/worker_base -u rabbitmq_utils/worker_transactions.py
+docker run -v $(pwd)/credentials:/app/credentials --entrypoint python3 -d --net="host" --name $1 dmitrydenisov/worker_base -u rabbitmq_utils/worker_transactions.py
