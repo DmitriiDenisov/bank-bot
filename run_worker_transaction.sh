58baf65477cc7e8d26efc0d918cf02b1c7acc4f4
DmitriiDenisov <dmitryhse@gmail.com>
2020-08-28 10:00:44 +0000

    edit run files

--- run_worker_transaction.sh
+++ run_worker_transaction.sh
@@ -1,2 +1,3 @@
+docker login
 docker pull dmitrydenisov/worker_base
-docker run --entrypoint python3 -d --net="host" --name $1 worker_base -u rabbitmq_utils/worker_transactions.py
+docker run --entrypoint python3 -d --net="host" --name $1 dmitrydenisov/worker_base -u rabbitmq_utils/worker_transactions.py
