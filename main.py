ed60d770a67e3853b7a9b306879995ad2b2ead36
Dmitry Denisov <36160558+DmitriiDenisov@users.noreply.github.com>
2021-02-26 02:03:02 +0400

    Update main.py

--- main.py
+++ main.py
@@ -287,4 +287,4 @@ def get_docs():
 if __name__ == '__main__':
     # host='0.0.0.0' allows global connections, namely http://<ip_addess>:5001
     # Or for local access: http://172.20.10.2:5001/
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=80)
