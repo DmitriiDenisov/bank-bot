e58dd48e2d4cf160c11f075f2e4aac1137f94671
Dmitry Denisov <36160558+DmitriiDenisov@users.noreply.github.com>
2021-02-26 09:53:22 +0000

    refactoring tests

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-# test main.py 
+# test main.py
 import json
 from main import app
 from environs import Env
@@ -13,10 +13,11 @@ TEST_TOKEN = env("TEST_TOKEN")
 
 def test_get_trans():
     response = client.get('/get_trans',
-                        headers={
-                            'key': TEST_TOKEN})
+                          headers={
+                              'key': TEST_TOKEN})
     assert response.status_code == 200
-    assert type(json.loads(response.data.decode('utf-8'))['results']) == type(list())
+    assert type(json.loads(response.data.decode('utf-8'))
+                ['results']) == type(list())
 
 
 def test_my_bal():
@@ -34,19 +35,21 @@ def test_my_bal():
 
 def test_own_transfer():
     response = client.post('/own_transfer',
-                        query_string={'curr_from': 'usd', 'curr_to': 'aed', 'amount': 1},
-                        # content_type='multipart/form-data',
-                        headers={
-                            'key': TEST_TOKEN})
+                           query_string={'curr_from': 'usd',
+                                         'curr_to': 'aed', 'amount': 1},
+                           # content_type='multipart/form-data',
+                           headers={
+                               'key': TEST_TOKEN})
     assert response.status_code == 200
     assert json.loads(response.data.decode('utf-8')) == {"message": "Success"}
 
     # Transfer it back. Important: will work until 1USD=3.67AED
     response = client.post('/own_transfer',
-                        query_string={'curr_from': 'aed', 'curr_to': 'usd', 'amount': 3.67},
-                        # content_type='multipart/form-data',
-                        headers={
-                            'key': TEST_TOKEN})
+                           query_string={'curr_from': 'aed',
+                                         'curr_to': 'usd', 'amount': 3.67},
+                           # content_type='multipart/form-data',
+                           headers={
+                               'key': TEST_TOKEN})
     assert response.status_code == 200
     assert json.loads(response.data.decode('utf-8')) == {"message": "Success"}
 
@@ -58,22 +61,26 @@ def test_topup_and_transaction():
                            headers={
                                'key': TEST_TOKEN})
     assert response.status_code == 200
-    assert json.loads(response.data.decode('utf-8')) == {"message": "TopUp successful!"}
+    assert json.loads(response.data.decode('utf-8')
+                      ) == {"message": "TopUp successful!"}
 
     # Also transact this amount to another test account just to revert TopUp back so other tests will run successfully
     response = client.post('/do_transaction',
-                        query_string={'customer_id_to': 33, 'amount': 10, 'currency': 'aed'},
-                        # content_type='multipart/form-data',
-                        headers={
-                            'key': TEST_TOKEN})
+                           query_string={'customer_id_to': 33,
+                                         'amount': 10, 'currency': 'aed'},
+                           # content_type='multipart/form-data',
+                           headers={
+                               'key': TEST_TOKEN})
     assert response.status_code == 200
-    assert json.loads(response.data.decode('utf-8')) == {"message": "Transaction made!"}
+    assert json.loads(response.data.decode('utf-8')
+                      ) == {"message": "Transaction made!"}
 
 
 def test_main_new():
     response = client.post('/',
                            content_type='multipart/form-data',
-                           data={'method': 'auth', 'email': 'dmitryhse@gmail.com', 'password': 'it_is_fake_password'},
+                           data={'method': 'auth', 'email': 'dmitryhse@gmail.com',
+                                 'password': 'it_is_fake_password'},
                            headers={
                                'key': TEST_TOKEN}
                            )
@@ -84,11 +91,11 @@ def test_main_new():
 
 def test_main():
     response = client.post('/',
-                        content_type='multipart/form-data',
-                        data={'method': 'sign-up', 'email': 'dmitryhse@gmail.com', 'password': 'fake', 'confirm': 'fake'},
-                        headers={
-                            'key': TEST_TOKEN}
-                        )
+                           content_type='multipart/form-data',
+                           data={'method': 'sign-up', 'email': 'dmitryhse@gmail.com',
+                                 'password': 'fake', 'confirm': 'fake'},
+                           headers={
+                               'key': TEST_TOKEN}
+                           )
     assert response.status_code == 400
     assert 'Email: User alredy exists!' in str(response.data)
-
